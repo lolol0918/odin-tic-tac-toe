3fea9db780335b158fd207a6738a31127d6925f5
Derek <derekdvalerio@gmail.com>
2025-09-06 13:28:11 +0800

    feat: add setMarker method to gameBoard and integrate Cell factory
    - Implemented Cell factory with getValue() and addMarker() methods
    - Updated gameBoard to initialize cells instead of empty arrays
    - Added setMarker(row, col, marker) to place markers on the board
    - Verified functionality with console.log and console.table

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // generate the gameBoard factory
 
-function gameBoard() {
+function Gameboard() {
     const rows = 3;
     const columns = 3;
     let board = [];
@@ -11,22 +11,49 @@ function gameBoard() {
         for (let i = 0; i < rows; i++) {
             board[i] = [];
             for (let j = 0; j < columns; j++) {
-                board[i].push([]);
+                // this part "[]" is subject to changes 
+                // just a placeholder
+                // need to change it later after implementing the cell object
+                board[i].push(Cell());
             }
         }
     };
 
     const getBoard = () => board;
 
+    const resetBoard = () => initBoard();
+
+    const setMarker = (row, col, marker) => {
+        console.log("hello");
+        board[row][col].addMarker(marker);
+        // returns true/false if move was valid
+    };
+
     initBoard();
 
-    return { getBoard };
+    return { getBoard, resetBoard, setMarker };
 }
 
 function Cell() {
-    // TODO
+    let value = "";
+
+    const getValue = () => value;
+
+    const addMarker = (marker) => {
+        if (value === "") {
+            value = marker;
+            return true; // returns true the cell is not yet occupied
+        }
+
+        return false; // fails because the cell is already occupied
+
+    };
+
+    return { getValue, addMarker };
 };
 
-const game = gameBoard();
+const game = Gameboard();
 
-console.table(game.getBoard());
+
+game.setMarker(0, 0, 'X');
+console.log(game.getBoard()[0][0].getValue());
